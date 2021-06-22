47fb35b8be1e061c6660d1e2dbe7d69865f1107f
cristian <cristian.ortiz@uda.cl>
2021-06-22 19:13:24 -0400

    env variables file

--- src/database.js
+++ src/database.js
@@ -1,9 +1,10 @@
+require("dotenv").config({ path: "vars.env" });
 const mysql = require("mysql");
 const mysqlConnection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "vrip_postgrado",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 });
 
 mysqlConnection.connect(function (err) {
